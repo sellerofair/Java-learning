6f4b4c10cb38d32586983bb41117fc08c0b4246c
Timur Akopyan <sinhrofazotronbl@inbox.ru>
2021-04-05 14:22:22 +0300

    Abstract Shape

--- Computer Science Center. Java. Base/Shapes/Shape.java
+++ Computer Science Center. Java. Base/Shapes/Shape.java
@@ -1,11 +1,11 @@
 package Shapes;
 
-public class Shape {
+public abstract class Shape {
     private final Color color;
 
     public Shape(Color color) { this.color = color; }
 
     public Color getColor() { return color; }
 
-    public double getArea() { return Double.NaN; }
+    public abstract double getArea();
 }
