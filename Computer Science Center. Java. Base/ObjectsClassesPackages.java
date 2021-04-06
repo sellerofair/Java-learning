0f4724c80d50a601b2a539507f03fe55bbc0de4e
Timur Akopyan <sinhrofazotronbl@inbox.ru>
2021-04-06 14:07:41 +0300

    AsciiCharSequence added in ObjectsClassesPackages

--- Computer Science Center. Java. Base/ObjectsClassesPackages.java
+++ Computer Science Center. Java. Base/ObjectsClassesPackages.java
@@ -2,12 +2,11 @@ public class ObjectsClassesPackages {
     public static void main(String[] args) {
         // Robot robot = new Robot(0, 0, Direction.UP);
         // moveRobot(robot, -3, 4);
-        ComplexNumber cn1 = new ComplexNumber(-2.3412,7.2434);
-        ComplexNumber cn2 = new ComplexNumber(2.3412,-7.2434);
-        ComplexNumber cn3 = new ComplexNumber(3.23124,1435.23);
-        System.out.println(cn1);
-        System.out.println(cn2);
-        System.out.println(cn3);
+        byte[] acs = {100, 101, 102, 103};
+        AsciiCharSequence acs1 = new AsciiCharSequence(acs);
+        System.out.println(acs1);
+        System.out.println(acs1.length());
+        System.out.println(acs1.subSequence(1, 3));
     }
 
 // 3.3 Class declaration
@@ -212,6 +211,53 @@ public class ObjectsClassesPackages {
             result.append(im > 0 ? " + " : " - ");
             result.append(Math.abs(im));
             result.append('i');
+            return result.toString();
+        }
+    }
+
+// 3.5 Abstract classes & Interfaces
+  // Step 8
+    /**
+     * Sequence of ASCII symbols.
+     */
+    public static class AsciiCharSequence implements CharSequence {
+        
+        byte[] sequence;
+
+        public AsciiCharSequence(byte[] seq) {
+            sequence = new byte[seq.length];
+            for (int i = 0; i < seq.length; i++) {
+                sequence[i] = seq[i];
+            }
+        }
+
+        @Override
+        public CharSequence subSequence(int arg0, int arg1) {
+            byte[] subSeq = new byte[arg1 - arg0];
+            for (int i = 0; i < subSeq.length; i++) {
+                subSeq[i] = sequence[i + arg0];
+            }
+
+            return new AsciiCharSequence(subSeq);
+        }
+
+        @Override
+        public char charAt(int arg0) {
+            return (char) sequence[arg0];
+        }
+
+        @Override
+        public int length() {
+            return sequence.length;
+        }
+
+        @Override
+        public String toString() {
+            if (sequence.length == 0) { return ""; }
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < sequence.length; i++) {
+                result.append((char) sequence[i]);
+            }
             return result.toString();
         }
     }
