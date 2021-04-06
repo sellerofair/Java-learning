dde104bb09ce4275fc6dd0f8bdab2aeb129d1231
Timur Akopyan <sinhrofazotronbl@inbox.ru>
2021-04-06 11:01:39 +0300

    toString overrited in ComlexNumber

--- Computer Science Center. Java. Base/ObjectsClassesPackages.java
+++ Computer Science Center. Java. Base/ObjectsClassesPackages.java
@@ -2,17 +2,13 @@ public class ObjectsClassesPackages {
     public static void main(String[] args) {
         // Robot robot = new Robot(0, 0, Direction.UP);
         // moveRobot(robot, -3, 4);
-        ComplexNumber cn1 = new ComplexNumber(2.3412,7.2434);
-        ComplexNumber cn2 = new ComplexNumber(2.3412,7.2434);
+        ComplexNumber cn1 = new ComplexNumber(-2.3412,7.2434);
+        ComplexNumber cn2 = new ComplexNumber(2.3412,-7.2434);
         ComplexNumber cn3 = new ComplexNumber(3.23124,1435.23);
-        System.out.println("equals cn1 & cn2 true // " + cn1.equals(cn2));
-        System.out.println("equals cn2 & cn1 true // " + cn2.equals(cn1));
-        System.out.println("equals cn2 & cn3 false // " + cn2.equals(cn3));
-        System.out.println("equals cn3 & cn1 false // " + cn3.equals(cn1));
-        System.out.println("hash cn1 & cn2 true // " + (cn1.hashCode() == cn2.hashCode()));
-        System.out.println("hash cn2 & cn1 true // " + (cn2.hashCode() == cn1.hashCode()));
-        System.out.println("hash cn3 & cn1 false // " + (cn3.hashCode() == cn1.hashCode()));
-        System.out.println("hash cn3 & cn2 false // " + (cn3.hashCode() == cn2.hashCode()));    }
+        System.out.println(cn1);
+        System.out.println(cn2);
+        System.out.println(cn3);
+    }
 
 // 3.3 Class declaration
   // Step 12
@@ -207,6 +203,16 @@ public class ObjectsClassesPackages {
                 Double.doubleToLongBits(im)
             );
             return (int)h ^ (int)(h >>> 32);
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder result = new StringBuilder();
+            result.append(re);
+            result.append(im > 0 ? " + " : " - ");
+            result.append(Math.abs(im));
+            result.append('i');
+            return result.toString();
         }
     }
 }
