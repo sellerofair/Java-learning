cbf7e52f4e6c63f529dfb29084cd38dc3b2c8aad
Timur Akopyan <sinhrofazotronbl@inbox.ru>
2021-04-11 11:57:03 +0300

    Nickname refactored

--- TinkoffExams/Nickname/Main.java
+++ TinkoffExams/Nickname/Main.java
@@ -6,34 +6,40 @@ import java.util.Scanner;
 public class Main {
     
     public static void main(String[] args) throws IOException {
+
         Scanner scanner = null;
+
         try {
+
             scanner = new Scanner(System.in);
+
             String nick = scanner.nextLine();
+
             System.out.println(nickIsPretty(nick.toString()));
+
         } catch (Exception e) {
             System.out.println(e.getStackTrace());
         } finally {
             scanner.close();
         }
+
     }
 
     static String nickIsPretty(String nick) {
+
         for (int i = 0; i < nick.length() - 1; i++) {
-            if (isVowel(nick.charAt(i))) {
-                if (isVowel(nick.charAt(i + 1))) {
-                    return "NO";
-                }
-            } else {
-                if (!isVowel(nick.charAt(i + 1))) {
-                    return "NO";
-                }
-            }
+
+            if (isVowel(nick.charAt(i)) == isVowel(nick.charAt(i + 1))) { return "NO"; }
+
         }
+
         return "YES";
+
     }
 
     static boolean isVowel(char letter) {
+
         return "aeiouy".indexOf(letter) != -1;
+
     }
 }
