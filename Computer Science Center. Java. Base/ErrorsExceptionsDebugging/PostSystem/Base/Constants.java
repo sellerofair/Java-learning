f47e808bf3b801e450e6b60d3a81e2214706ad82
Timur Akopyan <sinhrofazotronbl@inbox.ru>
2021-04-28 17:45:07 +0300

    PostSystem added

---
+++ Computer Science Center. Java. Base/ErrorsExceptionsDebugging/PostSystem/Base/Constants.java
@@ -0,0 +1,7 @@
+package ErrorsExceptionsDebugging.PostSystem.Base;
+
+public class Constants {
+    public static final String AUSTIN_POWERS = "Austin Powers";
+    public static final String WEAPONS = "weapons";
+    public static final String BANNED_SUBSTANCE = "banned substance";
+}
